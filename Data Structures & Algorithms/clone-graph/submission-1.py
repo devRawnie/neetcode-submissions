4d5f7594984c8353c4ba244cf49a6b96e87cafb9
ROHIT SHARMA <rohitsdec4@gmail.com>
2026-08-13 06:22:13 +0530

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        visited = set()
+        node_map = {}
+        def dfs(root):
+            if not root:
+                return None
+
+            if root.val in visited:
+                return node_map[root.val]
+
+            visited.add(root.val)
+            n = Node(root.val)
+            node_map[root.val] = n
+            for neigh in root.neighbors:
+                n1 = dfs(neigh)
+                if n1:
+                    n.neighbors.append(n1)
+
+            return n
+
+        new_root = dfs(node)
+        return new_root
